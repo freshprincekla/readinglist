fa2fbdd35a25fbe3886e56d8787d0458ccd362ff
Brian Serumaga <robinahkasoma@Robinahs-iMac.fios-router.home>
2018-09-02 16:47:53 -0400

    Added error handling for when author of book is not provided

--- src/book.js
+++ src/book.js
@@ -1,29 +1,40 @@
 import React, { Component } from 'react';
 
 class Book extends Component{
+    constructor(props){
+        super(props);
+        this.changeShelf = this.changeShelf.bind(this);
+    };
+    changeShelf(event){
+        const { book } = this.props;
+        if(book.shelf !== event.target.value){
+            this.props.changeShelf(book,event.target.value);
+        };
+    };
+
     render(){
         const { book } = this.props
         return(
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, background: '#0080008a' }}></div>
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.previewLink})` }}></div>
                         <div className="book-shelf-changer">
-                            <select>
+                            <select onChange={this.changeShelf}>
                                 <option value="move" disabled>Move to...</option>
+                                <option value="none">None</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
-                                <option value="none">None</option>
                             </select>
                         </div>
                     </div>
                     <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors.map(name => name)}</div>
+                    <div className="book-authors">{book.authors?book.authors.map(name => name):null}</div>
                 </div>
             </li>
-        )
-    }
-}
+        );
+    };
+};
 
 export default Book;
