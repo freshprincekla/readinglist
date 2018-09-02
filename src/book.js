7f4be2ef905d25ddebb3c308257618b9b2a56e12
Brian Serumaga <robinahkasoma@Robinahs-iMac.fios-router.home>
2018-09-01 22:11:41 -0400

    Added logic to loop over relevant books

--- src/book.js
+++ src/book.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react';
 
 class Book extends Component{
     render(){
+        const { book } = this.props
         return(
-            <div>
-            </div>
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, background: '#0080008a' }}></div>
+                        <div className="book-shelf-changer">
+                            <select>
+                                <option value="move" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors.map(name => name)}</div>
+                </div>
+            </li>
         )
     }
 }
