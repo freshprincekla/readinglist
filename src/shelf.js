380b1f8a7629f784b85b77c326dfde8e1f6520a4
Brian Serumaga <robinahkasoma@Robinahs-iMac.fios-router.home>
2018-09-02 16:50:36 -0400

    Formatted book component for easier reading

--- src/shelf.js
+++ src/shelf.js
@@ -4,17 +4,20 @@ import Book from './book';
 
 class BookShelf extends Component{
     render () {
-        const {books, catergory} = this.props
+        const {books, catergory, changeShelf} = this.props
         return(
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{catergory.title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {
+                        {   
                             books.map( book =>{
                                 if(book.shelf === catergory.value){
                                     return(
-                                        <Book key={book.id} book={book}/>
+                                        <Book 
+                                            key={book.id} 
+                                            book={book} 
+                                            changeShelf={changeShelf}/>
                                     );
                                 }return false
                             })
